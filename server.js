aba9a4eba6f72ff4f37ced627d21ac24b71001c0
Sean Jun <seanjun21@gmail.com>
2016-10-14 22:25:21 -0700

    one more time

--- server.js
+++ server.js
@@ -1,53 +1,52 @@
 /*---------- DEPENDENCIES ----------*/
-var express = require( 'express' );
-var cors = require( 'cors' );
+var express = require('express');
+var cors = require('cors');
 var app = express();
 
-app.use( cors() );
+app.use(cors());
 app.use(express.static('./build'));
 
-var mongoose = require( 'mongoose' );
-var DATA = require( './data.js' );
-var _ = require( 'lodash' );
+var DATA = require('./data.js');
+var _ = require('lodash');
 
 /*---------- GET REQUESTS ----------*/
-app.get( '/recipes', cors(), function( request, response ) {
-  response.status( 200 ).json( "" );
-} );
+app.get('/recipes', cors(), function (request, response) {
+    response.status(200).json("");
+});
 
-app.get( '/recipes/:ingredient', cors(), function( request, response ) {
-  // request = ingredient1|ingredient2;
-  var ingrtSplit = request.params.ingredient.split( '|' );
+app.get('/recipes/:ingredient', cors(), function (request, response) {
+    // request = ingredient1|ingredient2;
+    var ingrtSplit = request.params.ingredient.split('|');
 
-  var test = _.filter( DATA, function( o ) {
-    var patt = new RegExp( ingrtSplit[ 0 ], 'i' );
-    var res = patt.test( o.ingredients );
-    return res;
-  } );
+    var test = _.filter(DATA, function (o) {
+        var patt = new RegExp(ingrtSplit[0], 'i');
+        var res = patt.test(o.ingredients);
+        return res;
+    });
 
-  var test2 = _.filter( test, function( o ) {
-    var patt = new RegExp( ingrtSplit[ 1 ], 'i' );
-    var res = patt.test( o.ingredients );
-    return res;
-  } );
+    var test2 = _.filter(test, function (o) {
+        var patt = new RegExp(ingrtSplit[1], 'i');
+        var res = patt.test(o.ingredients);
+        return res;
+    });
 
-  response.status( 200 ).json( test2 );
-} );
+    response.status(200).json(test2);
+});
 
 /*------ SERVER SET-UP ------*/
-var runServer = function(callback) {
-  var port = process.env.PORT || 9000;
-  var server = app.listen(port, function() {
-    console.log('Listening on port ' + port);
-    if (callback) {
-      callback(server);
-    }
-  });
+var runServer = function (callback) {
+    var port = process.env.PORT || 9000;
+    var server = app.listen(port, function () {
+        console.log('Listening on port ' + port);
+        if (callback) {
+            callback(server);
+        }
+    });
 };
 
 /*-- RUN SERVER --*/
 if (require.main === module) {
-  runServer();
+    runServer();
 }
 
 exports.app = app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-var path = require( 'path' );
-
-var webpack = require( 'webpack' );
-
-var packageData = require( './package.json' );
-
-var filename = [ packageData.name, packageData.version, 'js' ];
+var path = require('path');
+var webpack = require('webpack');
+var packageData = require('./package.json');
+var filename = [packageData.name, packageData.version, 'js'];
 
 module.exports = {
-  entry: path.resolve( __dirname, packageData.main ),
+  entry: path.resolve(__dirname, 'src/js/index.jsx'),
   output: {
-    path: path.resolve( __dirname, 'build' ),
-    filename: filename.join( '.' )
+    path: path.resolve(__dirname, 'build'),
+    filename: filename.join('.')
   },
   devtool: 'source-map',
   module: {
-    loaders: [ {
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      loader: 'babel',
-      query: {
-        presets: [ 'es2015', 'react' ]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
-    } ]
+    ]
   },
   resolve: {
-    extensions: [ '', '.js', '.jsx' ]
-  },
+    extensions: ['', '.js', '.jsx']
+  }
 };
