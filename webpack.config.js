1a5e1051266de7fd5a74c805f150d52b09716f2c
Sean Jun <seanjun21@gmail.com>
2016-10-14 21:02:11 -0700

    engine added

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   entry: path.resolve( __dirname, packageData.main ),
   output: {
     path: path.resolve( __dirname, 'build' ),
-    filename: filename.join( '.' ),
+    filename: filename.join( '.' )
   },
   devtool: 'source-map',
   module: {
